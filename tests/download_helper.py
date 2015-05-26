cc7d6e03d5b08bfeebcb6e407af7d39d61f71c97
Joachim Metz <joachim.metz@gmail.com>
2015-05-26 22:02:56 +0200

    Code review: 234700043: Changes to review script to support github fork.

--- tests/download_helper.py
+++ tests/download_helper.py
@@ -257,7 +257,7 @@ class Log2TimelineGithubReleasesDownloadHelperTest(unittest.TestCase):
 
   _PROJECT_ORGANIZATION = u'log2timeline'
   _PROJECT_NAME = u'dfvfs'
-  _PROJECT_VERSION = u'20150408'
+  _PROJECT_VERSION = u'20150523'
 
   def testGetLatestVersion(self):
     """Tests the GetLatestVersion functions."""
