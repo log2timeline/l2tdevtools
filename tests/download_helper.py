f2678076bcf2023dc7a6d50f72e64343abc7e96c
Joachim Metz <joachim.metz@gmail.com>
2017-03-06 21:15:37 +0100

    Changes after review

--- tests/download_helper.py
+++ tests/download_helper.py
@@ -180,7 +180,7 @@ class Log2TimelineGitHubReleasesDownloadHelperTest(unittest.TestCase):
   _PROJECT_ORGANIZATION = u'log2timeline'
   _PROJECT_NAME = u'dfvfs'
   # Hard-coded version to check parsing of GitHub page.
-  _PROJECT_VERSION = u'20160918'
+  _PROJECT_VERSION = u'20170116'
 
   def testGetLatestVersion(self):
     """Tests the GetLatestVersion functions."""
@@ -273,7 +273,7 @@ class SourceForgeDownloadHelperTest(unittest.TestCase):
 
   _PROJECT_NAME = u'pyparsing'
   # Hard-coded version to check parsing of SourceForge page.
-  _PROJECT_VERSION = u'2.1.9'
+  _PROJECT_VERSION = u'2.2.0'
 
   def testGetLatestVersion(self):
     """Tests the GetLatestVersion functions."""
