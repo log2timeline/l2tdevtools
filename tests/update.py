5e8f8060e8ee6ba2ade7be622b532c999f296371
Joachim Metz <joachim.metz@gmail.com>
2017-06-01 07:41:17 +0200

    Changes to improve dpkg build target #4

--- tests/update.py
+++ tests/update.py
@@ -13,7 +13,7 @@ class GithubRepoDownloadHelperTest(unittest.TestCase):
   _DOWNLOAD_URL = u'https://github.com/ForensicArtifacts/artifacts/releases'
 
   _PROJECT_NAME = u'artifacts'
-  _PROJECT_VERSION = u'20170513'
+  _PROJECT_VERSION = u'20170522'
 
   def testGetPackageDownloadURLs(self):
     """Tests the GetPackageDownloadURLs function."""
