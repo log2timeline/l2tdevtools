fe471eeeeeac704d194c463fdcc64ff28094718f
Joachim Metz <joachim.metz@gmail.com>
2017-07-30 11:30:35 +0200

    Added dependencies of requests

--- tests/update.py
+++ tests/update.py
@@ -33,7 +33,7 @@ class DependencyUpdaterTest(unittest.TestCase):
   """Tests for the dependency updater class."""
 
   _PROJECT_NAME = u'dfvfs'
-  _PROJECT_VERSION = u'20170406'
+  _PROJECT_VERSION = u'20170723'
 
   def testGetPackageFilenamesAndVersions(self):
     """Tests the GetPackageFilenamesAndVersions function."""
