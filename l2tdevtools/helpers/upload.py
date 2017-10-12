e437175c60bcb4f1126310c448585453a1793e5e
Daniel White <onager@deerpie.com>
2017-10-12 14:41:42 -0400

    Add review script functionality to l2tdevtools

--- l2tdevtools/helpers/upload.py
+++ l2tdevtools/helpers/upload.py
@@ -79,7 +79,8 @@ class UploadHelper(cli.CLIHelper):
     self._access_token = None
     self._email_address = email_address
     self._no_browser = no_browser
-    self._upload_py_path = os.path.join(__file__, '..', 'lib', 'upload.py')
+    self._upload_py_path = os.path.join(
+      os.path.dirname(__file__), '..', 'lib', 'upload.py')
     self._xsrf_token = None
 
   def _GetReviewer(self, project_name):
