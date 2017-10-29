8026a308709983754eea8c685da638ec828cb4a6
Joachim Metz <joachim.metz@gmail.com>
2017-10-29 07:25:30 +0100

    deps2

--- l2tdevtools/dependencies.py
+++ l2tdevtools/dependencies.py
@@ -105,7 +105,7 @@ class DependencyHelper(object):
   """Dependency helper.
 
   Attributes:
-    dependencies (list[DependencyDefinition]): dependencies.
+    dependencies (dict[str, DependencyDefinition]): dependencies.
   """
 
   _VERSION_NUMBERS_REGEX = re.compile(r'[0-9.]+')
