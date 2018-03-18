19be85e0128560e2f0cdfc1d1faa0a12cc1637c8
Joachim Metz <joachim.metz@gmail.com>
2018-03-18 11:13:44 +0100

    Code review: 335650043: Added auto-generated comment to generated files #279

--- tests/build_helper.py
+++ tests/build_helper.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Tests for the helper for building projects from source."""
 
--- tools/dpkg-generate.py
+++ tools/dpkg-generate.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # pylint: disable=invalid-name
 """Script to generate dpkg packaging files."""
