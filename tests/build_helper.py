bfe0f28f62554a0405b37f3a388ac11bcca37972
Joachim Metz <joachim.metz@gmail.com>
2018-03-24 10:37:59 +0100

    Refactored build helpers into individual files #248

--- tests/build_helper.py
+++ tests/build_helper.py
@@ -7,23 +7,6 @@ from __future__ import unicode_literals
 import unittest
 
 
-# TODO: add PKGBuildHelper tests.
-# TODO: add ConfigureMakePKGBuildHelper tests.
-# TODO: add SetupPyPKGBuildHelper tests.
-
-# TODO: add BaseRPMBuildHelper tests.
-# TODO: add RPMBuildHelper tests.
-# TODO: add ConfigureMakeRPMBuildHelper tests.
-# TODO: add SetupPyRPMBuildHelper tests.
-
-# TODO: add SRPMBuildHelper tests.
-# TODO: add ConfigureMakeSRPMBuildHelper tests.
-# TODO: add SetupPySRPMBuildHelper tests.
-
-# TODO: add SourceBuildHelper tests.
-# TODO: add ConfigureMakeSourceBuildHelper tests.
-# TODO: add SetupPySourceBuildHelper tests.
-
 # TODO: add BuildHelperFactory tests.
 
 
