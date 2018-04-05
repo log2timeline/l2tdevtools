c222040fe0eef0057514f8ce7c47a0f1edce7660
Joachim Metz <joachim.metz@gmail.com>
2018-04-05 05:50:36 +0200

    Code review: 340630043: Merged Travis-CI script writers and clean up

--- l2tdevtools/dependency_writers/travis.py
+++ l2tdevtools/dependency_writers/travis.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Writer for install.sh files."""
+"""Writers for Travis-CI script files."""
 
 from __future__ import unicode_literals
 
@@ -141,6 +141,58 @@ class TravisInstallScriptWriter(interface.DependencyFileWriter):
     file_content.extend(self._FILE_FOOTER)
 
     file_content = '\n'.join(file_content)
+    file_content = file_content.encode('utf-8')
+
+    with open(self.PATH, 'wb') as file_object:
+      file_object.write(file_content)
+
+
+class TravisRunTestsScriptWriter(interface.DependencyFileWriter):
+  """Travis-CI runtests.sh file writer."""
+
+  _TEMPLATE_FILE = os.path.join('data', 'templates', 'runtests.sh')
+
+  PATH = os.path.join('config', 'travis', 'runtests.sh')
+
+  def Write(self):
+    """Writes a runtests.sh file."""
+    paths_to_lint = [self._project_definition.name]
+    for path_to_lint in ('config', 'scripts', 'tests', 'tools'):
+      if os.path.isdir(path_to_lint):
+        paths_to_lint.append(path_to_lint)
+
+    paths_to_lint = sorted(paths_to_lint)
+    if os.path.isfile('setup.py'):
+      paths_to_lint.insert(0, 'setup.py')
+
+    template_mappings = {
+        'project_name': self._project_definition.name,
+        'paths_to_lint': ' '.join(paths_to_lint)
+    }
+
+    template_file = os.path.join(self._l2tdevtools_path, self._TEMPLATE_FILE)
+    file_content = self._GenerateFromTemplate(template_file, template_mappings)
+
+    file_content = file_content.encode('utf-8')
+
+    with open(self.PATH, 'wb') as file_object:
+      file_object.write(file_content)
+
+
+class TravisRunWithTimeoutScriptWriter(interface.DependencyFileWriter):
+  """Travis-CI run_with_timeout.sh file writer."""
+
+  _TEMPLATE_FILE = os.path.join('data', 'templates', 'run_with_timeout.sh')
+
+  PATH = os.path.join('config', 'travis', 'run_with_timeout.sh')
+
+  def Write(self):
+    """Writes a run_with_timeout.sh file."""
+    template_mappings = {}
+
+    template_file = os.path.join(self._l2tdevtools_path, self._TEMPLATE_FILE)
+    file_content = self._GenerateFromTemplate(template_file, template_mappings)
+
     file_content = file_content.encode('utf-8')
 
     with open(self.PATH, 'wb') as file_object:
