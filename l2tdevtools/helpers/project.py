6daabbac0a06543d26d441ebf3ea6b440c8877c6
Joachim Metz <joachim.metz@gmail.com>
2018-05-16 08:07:55 +0200

    Updated supported projects

--- l2tdevtools/helpers/project.py
+++ l2tdevtools/helpers/project.py
@@ -71,10 +71,11 @@ class ProjectHelper(cli.CLIHelper):
 
   # yapf: enable
 
+  # Note that review is submodule name of l2tdevtools not a stand-alone project.
   SUPPORTED_PROJECTS = frozenset([
       'acstore', 'artifacts', 'dfdatetime', 'dfkinds', 'dfvfs', 'dfwinreg',
       'dftimewolf', 'dtfabric', 'dtformats', 'esedb-kb', 'l2tdevtools',
-      'l2tdocs', 'l2tpreg', 'plaso', 'winevt-kb', 'winreg-kb'])
+      'l2tdocs', 'l2tpreg', 'plaso', 'review', 'winevt-kb', 'winreg-kb'])
 
   def __init__(self, project_path):
     """Initializes a project helper.
