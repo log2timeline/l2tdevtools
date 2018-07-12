e356ae5b94e6a845d53b1c08e9aed1d4508706e7
Joachim Metz <joachim.metz@gmail.com>
2018-07-12 10:42:11 +0200

    Changes to support latests pypi URLs #340

--- l2tdevtools/download_helpers/pypi.py
+++ l2tdevtools/download_helpers/pypi.py
@@ -24,9 +24,10 @@ class PyPIDownloadHelper(project.ProjectDownloadHelper):
       ValueError: if download URL is not supported.
     """
     url_segments = download_url.split('/')
-    if (len(url_segments) < 5 or url_segments[2] != 'pypi.python.org' or
-        url_segments[3] != 'pypi'):
-      raise ValueError('Unsupported download URL.')
+    if (len(url_segments) < 5 or url_segments[2] != 'pypi.org' or
+        url_segments[3] != 'project'):
+      raise ValueError('Unsupported download URL: {0:s}.'.format(
+          download_url))
 
     super(PyPIDownloadHelper, self).__init__(download_url)
     self._project_name = url_segments[4]
@@ -47,7 +48,7 @@ class PyPIDownloadHelper(project.ProjectDownloadHelper):
       if earliest_version and earliest_version[0] == '==':
         return '.'.join(earliest_version[1:])
 
-    download_url = 'https://pypi.python.org/simple/{0:s}'.format(
+    download_url = 'https://pypi.org/project/{0:s}#files'.format(
         self._project_name)
 
     page_content = self.DownloadPageContent(download_url)
@@ -56,8 +57,10 @@ class PyPIDownloadHelper(project.ProjectDownloadHelper):
 
     versions = {}
     expression_string = (
-        r'.*/packages/.*/{0:s}-(?P<version>[\d\.\!]*(post\d+)?)'
-        r'\.(tar\.bz2|tar\.gz|zip)#').format(self._project_name)
+        r'"https://files.pythonhosted.org/packages/.*/.*/.*/'
+        r'{0:s}-(?P<version>[\d\.\!]*(post\d+)?)'
+        r'\.(tar\.bz2|tar\.gz|zip)"').format(self._project_name)
+
     for match in re.finditer(expression_string, page_content):
       version_string = match.group('version')
       if version_string:
@@ -97,29 +100,25 @@ class PyPIDownloadHelper(project.ProjectDownloadHelper):
     Returns:
       str: download URL of the project or None if not available.
     """
-    download_url = (
-        'https://pypi.python.org/pypi/{0:s}/{1!s}').format(
-            self._project_name, project_version)
+    download_url = 'https://pypi.org/project/{0:s}/{1!s}'.format(
+        self._project_name, project_version)
 
     page_content = self.DownloadPageContent(download_url)
     if not page_content:
       return None
 
     # The format of the project download URL is:
-    # https://pypi.python.org/packages/.*/{project name}-{version}.{extension}
+    # https://files.pythonhosted.org/packages/.*/.*/.*/{project name}-{version}.{extension}
     expression_string = (
-        '(https://(pypi.python.org|files.pythonhosted.org)/packages/.*/'
+        '(https://files.pythonhosted.org/packages/.*/.*/.*/'
         '{0:s}-{1!s}[.](tar[.]bz2|tar[.]gz|zip))').format(
             self._project_name, project_version)
     matches = re.findall(expression_string, page_content)
 
-    if matches:
-      return matches[0][0]
+    if not matches:
+      return None
 
-    return (
-        'https://pypi.python.org/packages/source/{0:s}/{1:s}/'
-        '{1:s}-{2:s}.tar.gz').format(
-            self._project_name[0], self._project_name, project_version)
+    return matches[0][0]
 
   def GetProjectIdentifier(self):
     """Retrieves the project identifier for a given project name.
@@ -127,4 +126,4 @@ class PyPIDownloadHelper(project.ProjectDownloadHelper):
     Returns:
       str: project identifier.
     """
-    return 'org.python.pypi.{0:s}'.format(self._project_name)
+    return 'org.pypi.{0:s}'.format(self._project_name)
