584b0570340191c8c9febd11a4e2855282dbe03f
Joachim Metz <joachim.metz@gmail.com>
2018-09-02 14:55:18 +0200

    Added 64-bit Python tests on AppVeyor

--- l2tdevtools/dependency_writers/travis_yml.py
+++ l2tdevtools/dependency_writers/travis_yml.py
@@ -43,10 +43,11 @@ class TravisYMLWriter(interface.DependencyFileWriter):
     template_data = self._GenerateFromTemplate('header', template_mappings)
     file_content.append(template_data)
 
-    if self._project_definition.name in ('l2tpreg', 'plaso'):
+    if self._project_definition.name in ('dfvfs', 'plaso'):
       template_data = self._GenerateFromTemplate('jenkins', template_mappings)
       file_content.append(template_data)
 
+    if self._project_definition.name in ('l2tpreg', 'plaso'):
       template_data = self._GenerateFromTemplate(
           'allow_failures', template_mappings)
       file_content.append(template_data)
