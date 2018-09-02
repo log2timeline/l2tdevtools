27891cfaf881360cdb588bf4862e3cd0fe2d113d
Joachim Metz <joachim.metz@gmail.com>
2018-09-02 07:30:18 +0200

    Improved version check in manage script

--- tests/download_helpers/pypi.py
+++ tests/download_helpers/pypi.py
@@ -22,7 +22,7 @@ class PyPIDownloadHelperTest(test_lib.BaseTestCase):
   _DOWNLOAD_URL = 'https://pypi.org/project/dfvfs'
 
   _PROJECT_NAME = 'dfvfs'
-  _PROJECT_VERSION = '20180827'
+  _PROJECT_VERSION = '20180831'
 
   def testGetLatestVersion(self):
     """Tests the GetLatestVersion functions."""
