ecc90d01b3eed21797635443c87ae824abfb0788
Joachim Metz <joachim.metz@gmail.com>
2018-11-21 16:44:01 +0100

    Added added latest version support to PyPI download helper #478 (#492)
    * Added added latest version support to PyPI download helper #478

--- l2tdevtools/download_helpers/pypi.py
+++ l2tdevtools/download_helpers/pypi.py
@@ -32,6 +32,52 @@ class PyPIDownloadHelper(project.ProjectDownloadHelper):
     super(PyPIDownloadHelper, self).__init__(download_url)
     self._project_name = url_segments[4]
 
+  def _GetAvailableVersions(self, version_strings):
+    """Determines the available versions from version string matched.
+
+    This function will split the version string and convert every digit into
+    an integer. These lists of integers allow us to reliable compare versions.
+    A string compare of version strings will yield an incorrect result.
+
+    Args:
+      version_strings (list[str]): version strings.
+
+    Returns:
+      dict[str, [int]]: available versions where the key is the original
+          version string and the value the individual integers of
+          the digits in the version string.
+    """
+    available_versions = {}
+
+    for version_string in version_strings:
+      if not version_string:
+        continue
+
+      # We need to support PEP440 epoch versioning, which only newer versions
+      # of setuptools support. So this is a bit of hack to handle epochs
+      # but still use setuptools to handle most of the version matching.
+      epoch, _, epoch_version_string = version_string.partition('!')
+      if not epoch_version_string:
+        # Per PEP440, if there is no epoch specified, the epoch is 0.
+        epoch_version_string = epoch
+        epoch = 0
+      else:
+        epoch = int(epoch, 10)
+
+      # Note that pkg_resources.parse_version() returns an instance of
+      # pkg_resources.SetuptoolsVersion.
+      version_object = pkg_resources.parse_version(epoch_version_string)
+
+      # Convert the result of map() into a list for Python 3.
+      comparable_version = version_object.base_version.split('.')
+      comparable_version = list(map(int, comparable_version))
+      comparable_version.insert(0, epoch)
+
+      # Add the epoch to the version string for comparison.
+      available_versions[version_string] = comparable_version
+
+    return available_versions
+
   def GetLatestVersion(self, unused_project_name, version_definition):
     """Retrieves the latest version number for a given project name.
 
@@ -43,10 +89,15 @@ class PyPIDownloadHelper(project.ProjectDownloadHelper):
     Returns:
       str: latest version number or None if not available.
     """
+    earliest_version = None
+    latest_version = None
+
     if version_definition:
       earliest_version = version_definition.GetEarliestVersion()
       if earliest_version and earliest_version[0] == '==':
         return '.'.join(earliest_version[1:])
+
+      latest_version = version_definition.GetLatestVersion()
 
     download_url = 'https://pypi.org/project/{0:s}#files'.format(
         self._project_name)
@@ -55,40 +106,19 @@ class PyPIDownloadHelper(project.ProjectDownloadHelper):
     if not page_content:
       return None
 
-    versions = {}
     expression_string = (
         r'"https://files.pythonhosted.org/packages/.*/.*/.*/'
-        r'{0:s}-(?P<version>[\d\.\!]*(post\d+)?)'
-        r'\.(tar\.bz2|tar\.gz|zip)"').format(self._project_name)
+        r'{0:s}-([\d\.\!]*(post\d+)?)\.(tar\.bz2|tar\.gz|zip)"').format(
+            self._project_name)
 
-    for match in re.finditer(expression_string, page_content):
-      version_string = match.group('version')
-      if version_string:
-        # We need to support PEP440 epoch versioning, which only newer versions
-        # of setuptools support. So this is a bit of hack to handle epochs
-        # but still use setuptools to handle most of the version matching.
-        epoch, _, epoch_version_string = version_string.partition('!')
-        if not epoch_version_string:
-          # Per PEP440, if there is no epoch specified, the epoch is 0.
-          epoch_version_string = epoch
-          epoch = 0
-        else:
-          epoch = int(epoch, 10)
+    matches = re.findall(expression_string, page_content)
+    if not matches:
+      return None
 
-        # Note that pkg_resources.parse_version() returns an instance of
-        # pkg_resources.SetuptoolsVersion.
-        version_object = pkg_resources.parse_version(epoch_version_string)
-
-        # Add the epoch to the version string for comparison.
-        version_tuple = ((epoch, version_object), version_string)
-        versions[version_tuple] = version_string
-
-    latest_version = max(versions.keys())
-    if latest_version:
-      # Return the original string that defined the version
-      return versions[latest_version]
-
-    return None
+    available_versions = self._GetAvailableVersions([
+        match[0] for match in matches])
+    return self._GetLatestVersion(
+        earliest_version, latest_version, available_versions)
 
   def GetDownloadURL(self, unused_project_name, project_version):
     """Retrieves the download URL for a given project name and version.
