99824fd34f142dfb63dba90545aca980d3bcfd8e
Kristinn <kristinn@log2timeline.net>
2018-11-26 14:45:29 +0000

    Changing the name of PlasoScaffolder to l2tscaffolder. (#504)

--- l2tdevtools/helpers/project.py
+++ l2tdevtools/helpers/project.py
@@ -58,8 +58,8 @@ class ProjectHelper(cli.CLIHelper):
           'joachimmetz',
           'Onager',
           'rgayon']),
-      'PlasoScaffolder': frozenset([
-          'joachimmetz',
+      'l2tscaffolder': frozenset([
+          'kiddinn',
           'Onager'])}
 
   _REVIEWERS_DEFAULT = frozenset([
@@ -72,7 +72,7 @@ class ProjectHelper(cli.CLIHelper):
   SUPPORTED_PROJECTS = frozenset([
       'acstore', 'artifacts', 'dfdatetime', 'dfkinds', 'dfvfs', 'dfwinreg',
       'dftimewolf', 'dtfabric', 'dtformats', 'esedb-kb', 'l2tdevtools',
-      'l2tdocs', 'l2tpreg', 'plaso', 'PlasoScaffolder', 'review', 'vstools',
+      'l2tdocs', 'l2tpreg', 'plaso', 'l2tscaffolder', 'review', 'vstools',
       'winevt-kb', 'winreg-kb'])
 
   def __init__(self, project_path):
