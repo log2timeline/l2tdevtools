4421e0062e23714b37d359d017458f8adcb45818
Joachim Metz <joachim.metz@gmail.com>
2018-11-27 22:21:02 +0100

    Changes to check dependencies writer (#507)

---
+++ data/templates/check_dependencies-with_url.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Script to check for the availability and version of dependencies."""
+
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import sys
+
+# Change PYTHONPATH to include dependencies.
+sys.path.insert(0, '.')
+
+import utils.dependencies  # pylint: disable=wrong-import-position
+
+
+if __name__ == '__main__':
+  dependency_helper = utils.dependencies.DependencyHelper()
+
+  if not dependency_helper.CheckDependencies():
+    build_instructions_url = (
+        'https://github.com/log2timeline/plaso/wiki/Users-Guide')
+
+    print('See: {0:s} on how to set up ${project_name}.'.format(build_instructions_url))
+    print('')
+
+    sys.exit(1)
