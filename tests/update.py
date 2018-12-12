76fa6a64438e7d18ad851e5af3c6fb13fa357b07
Joachim Metz <joachim.metz@gmail.com>
2018-12-12 23:50:47 +0100

    Updated URL in check dependencies script template (#514)

--- tests/update.py
+++ tests/update.py
@@ -57,7 +57,7 @@ class DependencyUpdaterTest(test_lib.BaseTestCase):
   # pylint: disable=protected-access
 
   _PROJECT_NAME = 'dfvfs'
-  _PROJECT_VERSION = '20180831'
+  _PROJECT_VERSION = '20181202'
 
   def testGetPackageFilenamesAndVersions(self):
     """Tests the GetPackageFilenamesAndVersions function."""
