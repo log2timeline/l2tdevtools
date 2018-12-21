903e2b761ba05470e5b5203767fae7d73853fdaa
Joachim Metz <joachim.metz@gmail.com>
2018-12-21 08:25:09 +0100

    Migrated from trusty to xenial

--- tests/update.py
+++ tests/update.py
@@ -57,7 +57,7 @@ class DependencyUpdaterTest(test_lib.BaseTestCase):
   # pylint: disable=protected-access
 
   _PROJECT_NAME = 'dfvfs'
-  _PROJECT_VERSION = '20181202'
+  _PROJECT_VERSION = '20181215'
 
   def testGetAvailablePackages(self):
     """Tests the _GetAvailablePackages function."""
