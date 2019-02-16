0451cfdb9a5b2b8b139619c7d7e9961dffc21ec4
Joachim Metz <joachim.metz@gmail.com>
2019-02-16 14:58:31 +0100

    Removed pylint 1.x support (#583)

--- l2tdevtools/dependency_writers/pylint_rc.py
+++ l2tdevtools/dependency_writers/pylint_rc.py
@@ -11,11 +11,9 @@ from l2tdevtools.dependency_writers import interface
 class PylintRcWriter(interface.DependencyFileWriter):
   """Pylint.rc file writer."""
 
-  PATH = '.pylintrc'
+  _TEMPLATE_FILE = os.path.join('data', 'templates', '.pylintrc')
 
-  _PROJECTS_WITH_PYLINT2_SUPPORT = (
-      'artifacts', 'dfdatetime', 'dfvfs', 'dfwinreg', 'dtfabric', 'dtformats',
-      'plaso')
+  PATH = '.pylintrc'
 
   def Write(self):
     """Writes a .pylintrc file."""
@@ -23,13 +21,7 @@ class PylintRcWriter(interface.DependencyFileWriter):
 
     template_mappings = {'extension_pkg_whitelist': ','.join(dependencies)}
 
-    if self._project_definition.name in self._PROJECTS_WITH_PYLINT2_SUPPORT:
-      template_file = '.pylintrc2'
-    else:
-      template_file = '.pylintrc1'
-
-    template_file = os.path.join(
-        self._l2tdevtools_path, 'data', 'templates', template_file)
+    template_file = os.path.join(self._l2tdevtools_path, self._TEMPLATE_FILE)
     file_content = self._GenerateFromTemplate(template_file, template_mappings)
 
     file_content = file_content.encode('utf-8')
