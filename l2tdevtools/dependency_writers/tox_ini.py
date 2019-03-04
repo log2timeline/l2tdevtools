8e16c1855d52df9c2c7d1dae5e8b9447735343bb
Joachim Metz <joachim.metz@gmail.com>
2019-03-04 14:01:39 +0100

    Additional changes for running CI tests with Ubuntu on docker (#604)

--- l2tdevtools/dependency_writers/tox_ini.py
+++ l2tdevtools/dependency_writers/tox_ini.py
@@ -17,22 +17,7 @@ class ToxIniWriter(interface.DependencyFileWriter):
 
   def Write(self):
     """Writes a tox.ini file."""
-    python_dependencies = self._GetPyPIPythonDependencies(exclude_version=True)
-
-    test_dependencies = self._GetPyPITestDependencies(
-        python_dependencies, exclude_version=True)
-
-    # TODO: replace by test_dependencies.ini
-    test_dependencies.append('pytest')
-
-    # Note that tox will fail when trying to install the same dependency more
-    # than once.
-    test_dependencies = '\n'.join([
-        '    {0:s}'.format(dependency) for dependency in test_dependencies])
-
-    template_mappings = {
-        'project_name': self._project_definition.name,
-        'test_dependencies': test_dependencies}
+    template_mappings = {'project_name': self._project_definition.name}
 
     template_file = os.path.join(self._l2tdevtools_path, self._TEMPLATE_FILE)
     file_content = self._GenerateFromTemplate(template_file, template_mappings)
