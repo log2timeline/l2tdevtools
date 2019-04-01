36249568ad038420d39e781984f104ae501caea6
Joachim Metz <joachim.metz@gmail.com>
2019-04-01 08:51:38 +0200

    Moved pylint CI tests to Ubuntu 18.04 on Docker

--- tests/update.py
+++ tests/update.py
@@ -22,7 +22,7 @@ class GithubRepoDownloadHelperTest(test_lib.BaseTestCase):
   _DOWNLOAD_URL = 'https://github.com/ForensicArtifacts/artifacts/releases'
 
   _PROJECT_NAME = 'artifacts'
-  _PROJECT_VERSION = '20190113'
+  _PROJECT_VERSION = '20190320'
 
   def testGetPackageDownloadURLs(self):
     """Tests the GetPackageDownloadURLs function."""
