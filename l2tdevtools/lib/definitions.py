cd4a15c07a9e9493c2215125c099db365aa45850
Joachim Metz <joachim.metz@gmail.com>
2020-04-06 10:11:05 +0200

    Added definitions for default distributions (#802)

---
+++ l2tdevtools/lib/definitions.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""The l2tdevtools definitions."""
+
+from __future__ import unicode_literals
+
+
+# The default Fedora distribution
+DEFAULT_FEDORA_DISTRIBUTION = '31'
+
+# The default Ubuntu distribution
+DEFAULT_UBUNTU_DISTRIBUTION = 'bionic'
