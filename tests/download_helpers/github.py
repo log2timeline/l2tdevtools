1a331cefdcfa993d57978c66608e3adde7ca30ff
Joachim Metz <joachim.metz@gmail.com>
2020-07-09 18:39:25 +0200

    Updated MacOS CI tests to use Python 3.8

--- tests/download_helpers/github.py
+++ tests/download_helpers/github.py
@@ -101,7 +101,7 @@ class LibyalGitHubReleasesDownloadHelperTest(test_lib.BaseTestCase):
   _PROJECT_ORGANIZATION = 'libyal'
   _PROJECT_NAME = 'libevt'
   _PROJECT_STATUS = 'alpha'
-  _PROJECT_VERSION = '20200418'
+  _PROJECT_VERSION = '20200708'
 
   @classmethod
   def setUpClass(cls):
