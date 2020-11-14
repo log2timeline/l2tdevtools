92ac59f03a72e616e41166f06eb2647a03cc5e24
Joachim Metz <joachim.metz@gmail.com>
2020-11-14 12:29:09 +0100

    Updated dfVFS version in tests

--- tests/download_helpers/pypi.py
+++ tests/download_helpers/pypi.py
@@ -25,7 +25,7 @@ class PyPIDownloadHelperTest(test_lib.BaseTestCase):
   _GIT_URL = 'https://github.com/log2timeline/dfvfs.git'
 
   _PROJECT_NAME = 'dfvfs'
-  _PROJECT_VERSION = '20201107'
+  _PROJECT_VERSION = '20201114'
 
   @classmethod
   def setUpClass(cls):
