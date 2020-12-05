684870d14ccf04de52cba1611b3f568c29b44ab4
Joachim Metz <joachim.metz@gmail.com>
2020-12-05 06:57:58 +0100

    Removed Fedora 31 support (#953)

--- l2tdevtools/lib/definitions.py
+++ l2tdevtools/lib/definitions.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 
 
 # The default Fedora distribution
-DEFAULT_FEDORA_DISTRIBUTION = '31'
+DEFAULT_FEDORA_DISTRIBUTION = '32'
 
 # The default Ubuntu distribution
 DEFAULT_UBUNTU_DISTRIBUTION = 'bionic'
