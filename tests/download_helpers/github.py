8ed154da437ef8449a93cad14ad0e3242652aa3c
Joachim Metz <joachim.metz@gmail.com>
2021-05-30 09:56:39 +0200

    Changes for CI tests

--- tests/download_helpers/github.py
+++ tests/download_helpers/github.py
@@ -164,7 +164,7 @@ class Log2TimelineGitHubReleasesDownloadHelperTest(test_lib.BaseTestCase):
 
   _PROJECT_ORGANIZATION = 'log2timeline'
   _PROJECT_NAME = 'dfvfs'
-  _PROJECT_VERSION = '20210528'
+  _PROJECT_VERSION = '20210530'
 
   @classmethod
   def setUpClass(cls):
