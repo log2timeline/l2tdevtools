223fc97b3c812214d9d53deedbee98fdb6bf4535
Joachim Metz <joachim.metz@gmail.com>
2021-06-01 06:09:21 +0200

    Changes for CI tests

--- tests/download_helpers/pypi.py
+++ tests/download_helpers/pypi.py
@@ -19,8 +19,8 @@ class PyPIDownloadHelperTest(test_lib.BaseTestCase):
   _GIT_URL = 'https://github.com/log2timeline/dfvfs.git'
 
   _PROJECT_NAME = 'dfvfs'
-  _PROJECT_VERSION = '20210530'
-  _PYPI_VERSION = '20210530'
+  _PROJECT_VERSION = '20210531'
+  _PYPI_VERSION = '20210531'
 
   @classmethod
   def setUpClass(cls):
