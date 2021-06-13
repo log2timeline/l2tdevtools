84cedad40ce2801d45030cadbad64e168da1bc68
Joachim Metz <joachim.metz@gmail.com>
2021-06-13 10:48:17 +0200

    Removed create-pr from review script #203 (#1018)

--- tools/review.py
+++ tools/review.py
@@ -29,34 +29,6 @@ def Main():
           'Apply command to all files, currently only affects the lint '
           'command.'))
 
-  argument_parser.add_argument(
-      '--nobrowser', '--no-browser', '--no_browser', dest='no_browser',
-      action='store_true', default=False, help=(
-          'Disable the functionality to use the webbrowser to get the OAuth '
-          'token should be disabled.'))
-
-  argument_parser.add_argument(
-      '--noconfirm', '--no-confirm', '--no_confirm', dest='no_confirm',
-      action='store_true', default=False, help=(
-          'Do not ask for confirmation apply defaults.\n'
-          'WARNING: only use this when you are familiar with the defaults.'))
-
-  argument_parser.add_argument(
-      '--noedit', '--no-edit', '--no_edit', dest='no_edit', action='store_true',
-      default=False, help=(
-          'Do not allow edits from maintainers on the pull request.\n'
-          'Changing this can result in a more tedious code review.'))
-
-  argument_parser.add_argument(
-      '--offline', dest='offline', action='store_true', default=False, help=(
-          'The review script is running offline and any online check is '
-          'skipped.'))
-
-  help_message = 'Enable code style checking with yapf.'
-  argument_parser.add_argument(
-      '--enable-yapf', '--enable_yapf', dest='enable_yapf', action='store_true',
-      default=False, help=help_message)
-
   commands_parser = argument_parser.add_subparsers(dest='command')
 
   close_command_parser = commands_parser.add_parser('close')
@@ -65,9 +37,6 @@ def Main():
   close_command_parser.add_argument(
       'branch', action='store', metavar='BRANCH', default=None,
       help='name of the corresponding feature branch.')
-
-  commands_parser.add_parser('create-pr')
-  commands_parser.add_parser('create_pr')
 
   commands_parser.add_parser('lint')
 
@@ -92,11 +61,6 @@ def Main():
     elif ':' in str(feature_branch):
       _, _, feature_branch = feature_branch.rpartition(':')
 
-  if options.offline and options.command not in (
-      'lint', 'lint-test', 'lint_test', 'test'):
-    print('Cannot run: {0:s} in offline mode.'.format(options.command))
-    print_help_on_error = True
-
   if print_help_on_error:
     print('')
     argument_parser.print_help()
@@ -115,10 +79,7 @@ def Main():
       options.project_path,
       github_origin,
       feature_branch,
-      all_files=options.all_files,
-      no_browser=options.no_browser,
-      no_confirm=options.no_confirm,
-      no_edit=options.no_edit)
+      all_files=options.all_files)
 
   if not review_helper.InitializeHelpers():
     return False
@@ -126,24 +87,14 @@ def Main():
   if not review_helper.CheckLocalGitState():
     return False
 
-  if not options.offline and not review_helper.CheckRemoteGitState():
-    return False
-
   if not review_helper.Lint():
     return False
-
-  if options.enable_yapf:
-    if not review_helper.CheckStyle():
-      return False
 
   if not review_helper.Test():
     return False
 
   result = False
-  if options.command in ('create-pr', 'create_pr'):
-    result = review_helper.CreatePullRequest()
-
-  elif options.command == 'close':
+  if options.command == 'close':
     result = review_helper.Close()
 
   elif options.command in ('lint', 'lint-test', 'lint_test', 'test'):
