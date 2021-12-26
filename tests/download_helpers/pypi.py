1253cb99c3660d2cb7b40af2dfe528d1849a393a
Joachim Metz <joachim.metz@gmail.com>
2021-12-26 07:13:40 +0100

    Changes to dfImageTools build configuration

--- tests/download_helpers/pypi.py
+++ tests/download_helpers/pypi.py
@@ -19,8 +19,8 @@ class PyPIDownloadHelperTest(test_lib.BaseTestCase):
   _GIT_URL = 'https://github.com/log2timeline/dfvfs.git'
 
   _PROJECT_NAME = 'dfvfs'
-  _PROJECT_VERSION = '20211223'
-  _PYPI_VERSION = '20211223'
+  _PROJECT_VERSION = '20211224'
+  _PYPI_VERSION = '20211224'
 
   @classmethod
   def setUpClass(cls):
