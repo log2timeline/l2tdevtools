7b2be3cf40f1b1339d8b94f36798d9e2a1e30554
Joachim Metz <joachim.metz@gmail.com>
2021-12-28 04:02:28 +0100

    Changes to setup.py template

--- tests/download_helpers/pypi.py
+++ tests/download_helpers/pypi.py
@@ -19,8 +19,8 @@ class PyPIDownloadHelperTest(test_lib.BaseTestCase):
   _GIT_URL = 'https://github.com/log2timeline/dfvfs.git'
 
   _PROJECT_NAME = 'dfvfs'
-  _PROJECT_VERSION = '20211224'
-  _PYPI_VERSION = '20211224'
+  _PROJECT_VERSION = '20211227'
+  _PYPI_VERSION = '20211227'
 
   @classmethod
   def setUpClass(cls):
