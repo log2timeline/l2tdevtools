fb9b158b3aa2d5d18e95a55ea4490f71128a718b
Joachim Metz <joachim.metz@gmail.com>
2022-01-28 07:25:48 +0100

    Changes to better handle knowledge base projects

--- tests/download_helpers/github.py
+++ tests/download_helpers/github.py
@@ -169,7 +169,7 @@ class Log2TimelineGitHubReleasesDownloadHelperTest(test_lib.BaseTestCase):
 
   _PROJECT_ORGANIZATION = 'log2timeline'
   _PROJECT_NAME = 'dfvfs'
-  _PROJECT_VERSION = '20211228'
+  _PROJECT_VERSION = '20220127'
 
   @classmethod
   def setUpClass(cls):
