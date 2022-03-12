3b8fc6d0a22b3fdb81ecb791a7efc9cce89639e4
Joachim Metz <joachim.metz@gmail.com>
2022-03-12 17:47:46 +0100

    Added yamllint to CI tests templates (#1095)

--- l2tdevtools/dependency_writers/tox_ini.py
+++ l2tdevtools/dependency_writers/tox_ini.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Writer for tox.ini files."""
 
-import io
+import glob
 import os
 
 from l2tdevtools.dependency_writers import interface
@@ -40,26 +40,45 @@ class ToxIniWriter(interface.DependencyFileWriter):
     if self._project_definition.name.endswith('-kb'):
       python_module_name = ''.join([python_module_name[:-3], 'rc'])
 
-    paths_to_lint = []
+    paths_to_lint_python = []
+    paths_to_lint_yaml = []
 
     if os.path.isdir(python_module_name):
-      paths_to_lint.append(python_module_name)
+      paths_to_lint_python.append(python_module_name)
+
+      if glob.glob(os.path.join(
+          python_module_name, '**', '*.yaml'), recursive=True):
+        paths_to_lint_yaml.append(python_module_name)
+
+    if os.path.isdir('data'):
+      if glob.glob(os.path.join('data', '**', '*.yaml'), recursive=True):
+        paths_to_lint_yaml.append('data')
 
     if os.path.isdir('scripts'):
-      paths_to_lint.append('scripts')
-    elif os.path.isdir('tools'):
-      paths_to_lint.append('tools')
+      paths_to_lint_python.append('scripts')
+
+    if os.path.isdir('test_data'):
+      if glob.glob(os.path.join('test_data', '**', '*.yaml'), recursive=True):
+        paths_to_lint_yaml.append('test_data')
 
     if os.path.isdir('tests'):
-      paths_to_lint.append('tests')
+      paths_to_lint_python.append('tests')
 
-      envlist = 'py3{6,7,8,9,10},coverage,docs,pylint'
+      if glob.glob(os.path.join('tests', '**', '*.yaml'), recursive=True):
+        paths_to_lint_yaml.append('tests')
+
+    if os.path.isdir('tools'):
+      paths_to_lint_python.append('tools')
+
+    if os.path.isdir('docs'):
+      envlist = 'py3{6,7,8,9,10},coverage,docs,lint'
     else:
-      envlist = 'py3{6,7,8,9,10},coverage,pylint'
+      envlist = 'py3{6,7,8,9,10},coverage,lint'
 
     template_mappings = {
         'envlist': envlist,
-        'paths_to_lint': ' '.join(sorted(paths_to_lint)),
+        'paths_to_lint_python': ' '.join(sorted(paths_to_lint_python)),
+        'paths_to_lint_yaml': ' '.join(sorted(paths_to_lint_yaml)),
         'project_name': self._project_definition.name,
         'python_module_name': python_module_name}
 
@@ -73,11 +92,16 @@ class ToxIniWriter(interface.DependencyFileWriter):
           'testenv_docs', template_mappings)
       file_content.append(template_data)
 
+    if paths_to_lint_yaml:
+      template_name = 'testenv_lint-with_yaml'
+    else:
+      template_name = 'testenv_lint'
+
     template_data = self._GenerateFromTemplate(
-        'testenv_pylint', template_mappings)
+        template_name, template_mappings)
     file_content.append(template_data)
 
     file_content = ''.join(file_content)
 
-    with io.open(self.PATH, 'w', encoding='utf-8') as file_object:
+    with open(self.PATH, 'w', encoding='utf-8') as file_object:
       file_object.write(file_content)
