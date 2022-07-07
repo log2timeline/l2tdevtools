0a32826ca88fccca6007056ba7a9a3a620a7d2b0
Joachim Metz <joachim.metz@gmail.com>
2022-07-07 20:06:27 +0200

    Updated Fedora versions and changes for pyzmq jammy build

--- l2tdevtools/lib/definitions.py
+++ l2tdevtools/lib/definitions.py
@@ -3,7 +3,7 @@
 
 
 # The default Fedora distribution
-DEFAULT_FEDORA_DISTRIBUTION = '34'
+DEFAULT_FEDORA_DISTRIBUTION = '35'
 
 # The default Ubuntu distribution
 DEFAULT_UBUNTU_DISTRIBUTION = 'bionic'
