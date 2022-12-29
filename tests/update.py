e4ec522d16ecb4b0626a2d629727610bc567c95e
Joachim Metz <joachim.metz@gmail.com>
2022-12-29 12:16:02 +0100

    Added project definition for libfcrypto

--- tests/update.py
+++ tests/update.py
@@ -47,7 +47,7 @@ class DependencyUpdaterTest(test_lib.BaseTestCase):
   # pylint: disable=protected-access
 
   _PROJECT_NAME = 'dfvfs'
-  _PROJECT_VERSION = '20220917'
+  _PROJECT_VERSION = '20221224'
 
   def testGetAvailablePackages(self):
     """Tests the _GetAvailablePackages function."""
