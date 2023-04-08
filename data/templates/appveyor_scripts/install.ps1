c0d8a828025367cabd3be55f5124d70b4c163f58
Joachim Metz <joachim.metz@gmail.com>
2023-04-08 19:28:52 +0200

    Changes to templates for CI tests

--- data/templates/appveyor_scripts/install.ps1
+++ data/templates/appveyor_scripts/install.ps1
@@ -1,10 +1,11 @@
 # Script to set up tests on AppVeyor Windows.
 
 $$Dependencies = "${dependencies}"
-$$Dependencies = $${Dependencies} -split " "
 
-If (i$$Dependencies.Count -gt 0)
+If ($$Dependencies.Length -gt 0)
 {
+	$$Dependencies = $${Dependencies} -split " "
+
 	$$Output = Invoke-Expression -Command "git clone https://github.com/log2timeline/l2tdevtools.git ..\l2tdevtools 2>&1 | %{ '$$_' }"
 	Write-Host ($${Output} | Out-String)
 
