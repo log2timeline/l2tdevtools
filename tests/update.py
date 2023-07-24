d1b44986282b12d94d84aef500df7c883f807388
Joachim Metz <joachim.metz@gmail.com>
2023-07-24 07:24:50 +0200

    Removed MSI support from update script

--- tests/update.py
+++ tests/update.py
@@ -20,7 +20,7 @@ class GithubRepoDownloadHelperTest(test_lib.BaseTestCase):
   _DOWNLOAD_URL = 'https://github.com/ForensicArtifacts/artifacts/releases'
 
   _PROJECT_NAME = 'artifacts'
-  _PROJECT_VERSION = '20221219'
+  _PROJECT_VERSION = '20230413'
 
   def testGetPackageDownloadURLs(self):
     """Tests the GetPackageDownloadURLs function."""
