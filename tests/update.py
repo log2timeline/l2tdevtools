e2a7cc0fbb80a7c62cd49d8576983d7b2e6f3f65
Joachim Metz <joachim.metz@gmail.com>
2023-09-30 12:14:27 +0200

    Changes to update script for zstd

--- tests/update.py
+++ tests/update.py
@@ -20,7 +20,7 @@ class GithubRepoDownloadHelperTest(test_lib.BaseTestCase):
   _DOWNLOAD_URL = 'https://github.com/ForensicArtifacts/artifacts/releases'
 
   _PROJECT_NAME = 'artifacts'
-  _PROJECT_VERSION = '20230413'
+  _PROJECT_VERSION = '20230928'
 
   def testGetPackageDownloadURLs(self):
     """Tests the GetPackageDownloadURLs function."""
