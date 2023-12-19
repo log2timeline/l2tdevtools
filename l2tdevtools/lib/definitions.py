909ff0931e9fb82a5cae338ae52c4a76eec2a698
Joachim Metz <joachim.metz@gmail.com>
2023-12-19 06:39:06 +0100

    Updated reference Fedora version

--- l2tdevtools/lib/definitions.py
+++ l2tdevtools/lib/definitions.py
@@ -3,7 +3,7 @@
 
 
 # The default Fedora distribution
-DEFAULT_FEDORA_DISTRIBUTION = '37'
+DEFAULT_FEDORA_DISTRIBUTION = '39'
 
 # The default Ubuntu distribution
 DEFAULT_UBUNTU_DISTRIBUTION = 'jammy'
