78445355b4517957d51f2960d450e15f157af80d
Joachim Metz <joachim.metz@gmail.com>
2024-03-03 14:00:48 +0100

    Changes for building fakeredis and Flor rpm

--- tests/update.py
+++ tests/update.py
@@ -20,7 +20,7 @@ class GithubRepoDownloadHelperTest(test_lib.BaseTestCase):
   _DOWNLOAD_URL = 'https://github.com/ForensicArtifacts/artifacts/releases'
 
   _PROJECT_NAME = 'artifacts'
-  _PROJECT_VERSION = '20240112'
+  _PROJECT_VERSION = '20240303'
 
   def testGetPackageDownloadURLs(self):
     """Tests the GetPackageDownloadURLs function."""
