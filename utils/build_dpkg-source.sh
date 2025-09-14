7931db994f254add014ef53c4f897ac384ee3439
Joachim Metz <joachim.metz@gmail.com>
2025-09-14 14:42:15 +0200

    Changes for building dpkgs

--- utils/build_dpkg-source.sh
+++ utils/build_dpkg-source.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-GPG_SESSION_FILE="/tmp/gpg-session-$$"
-
-touch ${GPG_SESSION_FILE}
-
-# Trigger GPG to unlock the passphrase for GPG agent.
-gpg -q -d ${GPG_SESSION_FILE}
-
 PYTHONPATH=. python3 ./tools/build.py --builds-directory ../l2tbuilds/dpkg-source --downloads-directory ../l2tbuilds/downloads $* dpkg-source
-
-rm -f ${GPG_SESSION_FILE}
