cc88327dce6251e9f4ff95021d2e904dd580fe17
Joachim Metz <joachim.metz@gmail.com>
2025-11-17 17:07:38 +0100

    Changes for Python 3.14 CI test target

--- l2tdevtools/spec_file.py
+++ l2tdevtools/spec_file.py
@@ -175,7 +175,7 @@ class RPMSpecFileGenerator(object):
           source_directory, '**', '__init__.py')):
         with open(version_file, 'r', encoding='utf8') as file_object:
           for line in file_object:
-            if ('__version__' in line or line.startswith('VERSION')) and '=' in line:
+            if '__version__' in line and '=' in line:
               version = line.strip().rsplit('=', maxsplit=1)[-1]
               version = version.strip().strip('\'').strip('"')
               # pytz sets __version__ to VERSION
